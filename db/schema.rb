23dff0cbfc193a1a3ecc5c3e2ce4e4aa46546023
Michael Amos <zuzuspetals@hotmail.co.uk>
2017-10-17 11:18:00 -0400

    9. reload database from scratch to debug

--- db/schema.rb
+++ db/schema.rb
@@ -10,6 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20171017150348) do
+
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "image_url"
+    t.string "author"
+    t.string "datetime"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
